62c56f2a791fdcef3b5d55be5de44c261d9639d1
Jensen Phetchampa <phetchampaj1@southernct.edu>
2025-11-16 05:37:07 +0000

    add details

--- public/index.js
+++ public/index.js
@@ -1,1 +1,16 @@
-x
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+      0: { slidesPerView: 1 },
+      600: { slidesPerView: 2 },
+      1000: { slidesPerView: 3 }
+    }
+  });
